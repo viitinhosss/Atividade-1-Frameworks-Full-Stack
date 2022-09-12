72f8595ec1fb8362001bfccbdf6cd01abbc0ff7f
Vitor Soares <100804420+viitinhosss@users.noreply.github.com>
2022-09-12 19:20:15 -0300

    Add files via upload

--- appdois/appdois/app.py
+++ appdois/appdois/app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, request, render_template
+from flaskext.mysql import MySQL
 app = Flask(__name__)
+ 
 
 app.config['MYSQL_DATABASE_USER'] = 'root' 
 app.config['MYSQL_DATABASE_PASSWORD'] = 'mudar123'
